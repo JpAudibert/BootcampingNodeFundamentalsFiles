0f38286c6925ee75f2c0c7adb8053f47141c23df
Joao Pedro Basso Audibert <hotsjoao@gmail.com>
2020-07-14 20:11:49 -0300

    creating transactions and categories

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,45 @@
+import { getRepository } from 'typeorm';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface NewTransactionRequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: NewTransactionRequestDTO): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+    const categoryRepository = getRepository(Category);
+
+    let checkCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!checkCategory) {
+      checkCategory = categoryRepository.create({ title: category });
+      await categoryRepository.save(checkCategory);
+    }
+
+    const newTransation = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: checkCategory.id,
+    });
+
+    transactionRepository.save(newTransation);
+
+    return newTransation;
   }
 }
 
